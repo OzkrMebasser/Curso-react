5df14f460c1a58b5a57af1b41c90f01dfd551455
= <danielmezauaeh@gmail.com>
2021-09-02 18:58:32 -0500

    useState listo

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //COMPONENTS
 import Header from "./components/Header";
+import Section from "./components/Section";
+//Estado
 
 const App = () => {
+  const [text, setText] = useState("Sin DarkMode");
+  const [pageInfo, setPageInfo] = useState([
+    {
+      title: "Título 1",
+      section: "Texto de prueba 1"
+    },
+    {
+      title: "Título 2",
+      section: "Texto de prueba 2"
+    },
+    {
+      title: "Título 3",
+      section: "Texto de prueba 3"
+    }
+  ]);
+
   return (
     <div className="App">
-      <Header titulo="Logo 1" numero="1" classCss="header1" />
-      <Header titulo="Logo 2" numero="2" classCss="header2" />
-      <Header titulo="Logo 3" numero="3" classCss="header3" />
+      <Header titulo="Logo 1" numero="1" classCss="header1" setText={setText} />
+      <h2 className="header__title">{text}</h2>
+      {pageInfo.map((section, index) => (
+        <Section title={section.title} section={section.section} key={index} />
+      ))}
     </div>
   );
 };
--- curso-react/src/components/Header.jsx
+++ curso-react/src/components/Header.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Header = ({ titulo, numero, classCss }) => {
-  //   const { titulo, item } = props;
-  //   console.log(props);
+//Estilos
+import "../styles/Header.css";
+
+//Componente
+const Header = ({ titulo, numero, classCss, setText }) => {
+  //Estado del componente
+  const [darkMode, setDarkMode] = useState(false);
+
+  //Funciones
+  // function handleDarkMode() {
+  //   if (darkMode === true) {
+  //     setDarkMode(false);
+  //   } else {
+  //     setDarkMode(true);
+  //   }
+  // }
+  const handleDarkMode = () => {
+    // if (darkMode === true) {
+    //   setDarkMode(false);
+    // } else {
+    //   setDarkMode(true);
+    // }
+    // setDarkMode(darkMode ? false : true);
+    setDarkMode(!darkMode);
+    setText(darkMode ? "Sin DarkMode" : "Con DarkMode");
+  };
+
   return (
-    <header className={classCss}>
-      <a href="#">{titulo}</a>
-      <ul>
-        <li>
-          <a href="#">Item {numero}</a>
-        </li>
-        <li>
-          <a href="#">Item {numero}</a>
-        </li>
-        <li>
-          <a href="#">Item {numero}</a>
-        </li>
-        <li>
-          <a href="#">Item {numero}</a>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className={!darkMode ? "normal-header" : "dark-header"}>
+        <a href="#">{titulo}</a>
+        <ul>
+          <li>
+            <a href="#">Item {numero}</a>
+          </li>
+          <li>
+            <a href="#">Item {numero}</a>
+          </li>
+          <li>
+            <a href="#">Item {numero}</a>
+          </li>
+          <li>
+            <a href="#">Item {numero}</a>
+          </li>
+
+          <li>
+            <button onClick={handleDarkMode}>Dark Mode</button>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 };
 
